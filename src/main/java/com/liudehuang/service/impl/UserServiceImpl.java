6b06c94a1c9e85130462044cc665f0b33b2bcea1
Administrator <Administrator@EDZ-20171213ZIJ>
2018-07-03 13:19:17 +0800

    第一种方式集成redis

--- src/main/java/com/liudehuang/service/impl/UserServiceImpl.java
+++ src/main/java/com/liudehuang/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.liudehuang.dao.RedisDao;
 import com.liudehuang.dao.UserDao;
 import com.liudehuang.entity.User;
 import com.liudehuang.service.UserService;
@@ -12,10 +13,14 @@ import com.liudehuang.service.UserService;
 public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserDao userDao;
-
+	@Autowired
+	private RedisDao redisDao;
 	@Override
 	public List<User> listUser() {
 		List<User> list = userDao.listUser();
+		for(User user:list) {
+			redisDao.set(String.valueOf(user.getId()), user.getUsername());
+		}
 		return list;
 	}
 
